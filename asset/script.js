e9c6d7aca995fa74ebbeef3c4136c38593cdc8c1
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-08-15 04:41:01 +0530

    update

--- asset/script.js
+++ asset/script.js
@@ -1,11 +1,15 @@
 let button = document.getElementById("btn");
 let input = document.getElementById("input");
 let task = document.getElementById("task");
+if(input.value.length==0){
+    task.style.display='none';
+}
 button.addEventListener('click', function () {
     if (input.value.length == 0) {
         alert('kindly Add a task!');
     }
     else {
+        task.style.display='block';
         task.innerHTML += `
         <div class="task">
         <span id="taskname">
@@ -18,10 +22,12 @@ button.addEventListener('click', function () {
         `;
         // button
         var current_tasks = document.querySelectorAll(".delete");
+        console.log(current_tasks);
         for (var i = 0; i < current_tasks.length; i++) {
             current_tasks[i].onclick = function () {
                 this.parentNode.remove();
             }
         }
+        input.value = "";
     }
 });
